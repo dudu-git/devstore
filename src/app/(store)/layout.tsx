c6943631cd7a80d582e1e3f6a315117a11a7c751
dudu-git <eduldjf@gmail.com>
2024-02-10 19:43:35 -0300

    feat: Cria o layout do header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,10 +1,13 @@
+import { Header } from '@/components/header';
+
 type Props = {
   children: React.ReactNode;
 };
 export default function StoreLayout({ children }: Props) {
   return (
-    <div>
-      <main>{children}</main>
+    <div className="mx-auto grid grid-rows-app min-h-screen w-full max-w-[1600px] gap-5 p-8">
+      <Header />
+      {children}
     </div>
   );
 }
