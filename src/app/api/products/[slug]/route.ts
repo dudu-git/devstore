b2d5f876c382a2d91945967c401b2c437ed0eb68
Eduardo Lima <eduldjf@gmail.com>
2024-02-11 09:59:02 -0300

    feat: Cria pagina de detalhe do produto

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,20 @@
+import data from '../data.json';
+
+import { z } from 'zod';
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } }
+) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const slug = z.string().parse(params.slug);
+
+  const product = data.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    return Response.json({ error: 'Produto não encontrado' }, { status: 400 });
+  }
+
+  return Response.json(product);
+}
