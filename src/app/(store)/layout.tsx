ee2468612f840967d778eca29cebfebea453a0d3
Eduardo Lima <eduldjf@gmail.com>
2024-02-11 12:28:13 -0300

    feat: cria context do carrinho de compra

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,13 +1,16 @@
 import { Header } from '@/components/header';
+import { CartProvider } from '@/contexts/cartContext';
 
 type Props = {
   children: React.ReactNode;
 };
 export default function StoreLayout({ children }: Props) {
   return (
-    <div className="mx-auto grid grid-rows-app min-h-screen w-full max-w-[1600px] gap-5 p-8">
-      <Header />
-      {children}
-    </div>
+    <CartProvider>
+      <div className="mx-auto grid grid-rows-app min-h-screen w-full max-w-[1600px] gap-5 p-8">
+        <Header />
+        {children}
+      </div>
+    </CartProvider>
   );
 }
