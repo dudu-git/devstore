8738eaa0ded289367d94c120dc79e859211dd10a
Eduardo Lima <eduldjf@gmail.com>
2024-02-11 16:59:51 -0300

    feat: loading da busca

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,19 @@
+import Skeleton from '@/components/skeleton';
+
 export default function SearchLoading() {
-  return <p>Carregando Search...</p>;
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultado para: <span className="font-semibold">...</span>
+      </p>
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+        <Skeleton className="h-96" />
+      </div>
+    </div>
+  );
 }
