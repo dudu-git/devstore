86d8bfa4c2f927089b6933bb2415c98ed21aa767
Eduardo Lima <eduldjf@gmail.com>
2024-02-11 10:57:20 -0300

    feat: Cria meta data dinâmico e cache de pagina estáticas.

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import Image from 'next/image';
 import React from 'react';
 
@@ -16,6 +17,21 @@ interface ProductProps {
   params: {
     slug: string;
   };
+}
+export async function generateMetadata({
+  params
+}: ProductProps): Promise<Metadata> {
+  const product = await getProduct(params.slug);
+  return {
+    title: product.title
+  };
+}
+export async function generateStaticParams() {
+  const response = await api('/products/featured');
+  const products: Product[] = await response.json();
+  return products.map((product) => {
+    return { slug: product.slug };
+  });
 }
 
 export default async function ProductPage({ params }: ProductProps) {
